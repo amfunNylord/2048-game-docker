7722429bd9bcf00dd7505ced409083346418bfe0
Aleksandr Mikhaylov <funnybulletgames@yandex.ru>
2025-02-05 12:02:24 +0300

    Добавил названия контейнеров

--- start.cmd
+++ start.cmd
@@ -2,5 +2,5 @@
 
 docker build -t amfunNylord/2048-game .
 
-docker run -d -p 8080:8080 amfunNylord/2048-game
-docker run -d -p 8081:8080 amfunNylord/2048-game
+docker run --name game1 -d -p 8082:8080 amfunNylord/2048-game
+docker run --name game2 -d -p 8081:8080 amfunNylord/2048-game
