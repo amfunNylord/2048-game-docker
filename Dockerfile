20d15d232082b4c53ce380f1153832be0c20b973
Aleksandr Mikhaylov <funnybulletgames@yandex.ru>
2025-02-06 10:46:03 +0300

    Исправил замечания

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM node:20-alpine
 WORKDIR /app
-COPY . .
+COPY data.txt .
+COPY game.js .
+COPY highscore.js .
+COPY index.html .
+COPY server.js .
+COPY style.css .
 EXPOSE 8080
 CMD ["node", "server.js"]
--- start.cmd
+++ start.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-docker build -t amfunNylord/2048-game .
+docker build -t amfunNylord/2048-game:1.0.0 .
 
-docker run --name game1 -d -p 8082:8080 amfunNylord/2048-game
-docker run --name game2 -d -p 8081:8080 amfunNylord/2048-game
+docker run --name game1 -d -p 8082:8080 amfunNylord/2048-game:1.0.0
+docker run --name game2 -d -p 8081:8080 amfunNylord/2048-game:1.0.0
